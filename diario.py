93fd4ea6aca71df5f3a0cb06c0c15b2c2007c511
“seu <renatoanndrade@gmail.com>
2026-04-25 17:11:17 -0300

    25-04-2026 17:10

--- diario.py
+++ diario.py
@@ -160,4 +160,4 @@ with pd.ExcelWriter(caminho_saida, engine='xlsxwriter') as w:
             worksheet.set_column(col_num, col_num, max_len + 2)
     
 print('diário salvo com sucesso')
-print('abra a pasta output, vai está o arquivo excel lá.')
+print('abra a pasta output, vai está o arquivo excel vai está lá.')
